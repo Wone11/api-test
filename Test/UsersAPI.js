d80b8fc8f852a38534b4b2dadb2122e7e36d4419
Wone11 <gelayegeremew@gmail.com>
2023-06-26 16:22:36 +0300

    done added to wait until async function doing its test

--- Test/UsersAPI.js
+++ Test/UsersAPI.js
@@ -8,7 +8,7 @@ const request  = supertest('https://gorest.co.in/public/api/')
  */
 const TOKEN= '6e6538c7d0a4d020ef666f855810527514ac75f4d31fe6ea1540df3c60f2b8cb'
 describe('users' , ()=>{
-    it('Get /users',()=>{
+    it('Get /users',(done)=>{
         request
         .get(`users?access-token=${TOKEN}`)
         .end((err,res)=>{
@@ -16,7 +16,8 @@ describe('users' , ()=>{
                 console.log('error during users data retrieval  : ' + err);
             }
             // console.log('data : ' + res.json);
-            expect(res.body).to.not.be.null;
+            expect(res.body).to.be.not.null;
+            done();
         })
     })
 })
